b864074c3a39465f564c5f7f90b1b99b85f5e87d
Glyn Normington <glyn.normington@gmail.com>
2024-03-01 16:03:02 +0000

    Throw together some code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import Control.Monad.Trans.List (ListT(..))
+import Data.Aeson
 import Lib
 
+
 main :: IO ()
-main = someFunc
+
+main = do
+    let ioAction = runListT $ do
+         nl <- root decode "{\"foo\": 123}" -- :: Maybe Value
+         fmap childWildcard nl
+             
+    -- Execute the IO action and print each nodelist
+    mapM_ print ioAction
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
+    ( Nodelist
+    , Query
+    , root
+    , childWildcard
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Aeson
+import Data.List
+
+type Nodelist = [Value] -- omit locations
+type Query = Value -> [Nodelist]
+ -- what does a non-deterministic query look like?
+
+-- root is a query corresponding to $
+-- It is deterministic, so it returns one nodelist.
+root :: Query
+root v = [[v]]
+
+childWildcard :: Query
+-- Object !Object	 
+-- Array !Array	 
+-- String !Text	 
+-- Number !Scientific	 
+--Bool !Bool	 
+--Null
+childWildcard (Object o) = permutations $ elems o
+childWildcard (Array a) = [toList a]
+childWildcard (String _) = [[]]
+childWildcard (Number _) = [[]]
+childWildcard (Bool _) = [[]]
+childWildcard Null = [[]]
