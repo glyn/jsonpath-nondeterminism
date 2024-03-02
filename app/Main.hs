7700d4e6f352be56ac935544cf7a6ef644df8a6b
Glyn Normington <glyn.normington@gmail.com>
2024-03-02 11:49:55 +0000

    Fix last compile error

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@
 
 module Main (main) where
 
-import Control.Monad.Trans.List (runListT)
 import Data.Aeson
 import Data.Maybe (fromMaybe)
 import Lib
@@ -11,10 +10,8 @@ import Lib
 main :: IO ()
 
 main = do
-    let ioAction = runListT $ do
-         nl <- root $ fromMaybe undefined decode "{\"foo\": 123}"
-         fmap childWildcard nl
-             
-    -- Execute the IO action and print each nodelist
-    mapM_ print ioAction
+         let result = do
+                        nl <- root $ fromMaybe undefined (decode "{\"foo\": 123}")
+                        fmap childWildcard nl in        
+            mapM_ print result
 
