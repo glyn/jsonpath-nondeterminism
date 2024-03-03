3d7ca1ecb585e09f49ac9619d2406815d2df4121
Glyn Normington <glyn.normington@gmail.com>
2024-03-03 03:03:47 +0000

    Generate a sequence of results

--- app/Main.hs
+++ app/Main.hs
@@ -10,8 +10,11 @@ import Lib
 main :: IO ()
 
 main = do
-         let result = do
-                        nl <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
-                        fmap childWildcard nl in        
-            mapM_ print result
-
+         mapM_ (mapM_ print) [
+            do
+              nl <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
+              fmap childWildcard nl,
+            do
+              nl <- root $ fromMaybe undefined (decode "[1,2]")
+              fmap childWildcard nl
+            ]
