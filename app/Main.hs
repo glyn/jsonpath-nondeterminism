7410112ceb6aa0d51ecf1eb4b56cbaa97a1261d4
Glyn Normington <glyn.normington@gmail.com>
2024-03-03 20:28:19 +0000

    Debugging

--- app/Main.hs
+++ app/Main.hs
@@ -17,9 +17,12 @@ main = do
               nl <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
               concatMap childWildcard nl,
             do -- [*][*] applied to an object of objects
+              -- DEBUG: we are in the list monad so the following binds nl to the Nodelist in the list returned from root
+              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
+
+              -- The following maps childWildcard over nl and concatenates the resultant nodelists.
                -- expect 4 possibilities, but got 8 with some duplicates
                -- TODO: understand this
-              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
               nl' :: Nodelist <- concatMap childWildcard nl
               concatMap childWildcard nl'
             ]
