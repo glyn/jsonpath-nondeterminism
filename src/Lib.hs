e2f38c81070d20eb905d7fb8e9ca8c85ebc6b99b
Glyn Normington <glyn.normington@gmail.com>
2024-03-03 03:13:29 +0000

    Improve comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,21 +11,19 @@ import Data.List (permutations)
 import Data.Vector (toList)
 
 type Nodelist = [Value] -- omit locations
+ -- Generalise a RFC 9535 query to return a list of possible results (nodelists).
+ -- If there is one item in the list, the query is deterministic.Applicative
+ -- If there is more than one item in the list, the query is non-deterministic.
 type Query = Value -> [Nodelist]
- -- what does a non-deterministic query look like?
 
 -- root is a query corresponding to $
 -- It is deterministic, so it returns one nodelist.
 root :: Query
 root v = [[v]]
 
+-- childWildcard is a query corresponding to [*]
+-- It is non-deterministic when applied to an object with more than one member.
 childWildcard :: Query
--- Object !Object	 
--- Array !Array	 
--- String !Text	 
--- Number !Scientific	 
---Bool !Bool	 
---Null
 childWildcard (Object o) = permutations $ elems o
 childWildcard (Array a) = [toList a]
 childWildcard (String _) = [[]]
