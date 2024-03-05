7e392e4220d774f13aa52b03311b6cee77d6bc30
Glyn Normington <glyn.normington@gmail.com>
2024-03-05 06:34:55 +0000

    Experiment

--- app/Main.hs
+++ app/Main.hs
@@ -12,17 +12,23 @@ import Lib
 main :: IO ()
 
 main = do
-         mapM_ (BSL.putStrLn . encode) [
-            do -- [*] applied to an object
-              nl <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
-              concatMap childWildcard nl,
-            do -- [*][*] applied to an object of objects
-              -- DEBUG: we are in the list monad so the following binds nl to the Nodelist in the list returned from root
-              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
+         mapM_ (BSL.putStrLn . encode) ([
+            -- do -- [*] applied to an object
+            --   nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
+            --   childWildcard nl
+            --   ,
+            -- do -- [*][*] applied to an object of objects
+            --   -- DEBUG: we are in the list monad so the following binds nl to the Nodelist in the list returned from root
+            --   nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
 
-              -- The following maps childWildcard over nl and concatenates the resultant nodelists.
-               -- expect 4 possibilities, but got 8 with some duplicates
-               -- TODO: understand this
-              nl' :: Nodelist <- concatMap childWildcard nl
-              concatMap childWildcard nl'
-            ]
+            --   -- The following maps childWildcard over nl and concatenates the resultant nodelists.
+            --    -- expect 4 possibilities, but got 8 with some duplicates
+            --    -- TODO: understand this
+            --   nl' :: Nodelist <- concatMap childWildcard nl
+            --   concatMap childWildcard nl'
+            ] :: [Nodelist])
+
+fred :: [Nodelist]
+fred = do
+         nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
+         childWildcard nl
--- src/Lib.hs
+++ src/Lib.hs
@@ -15,19 +15,25 @@ type Nodelist = [Value] -- omit locations
  -- Generalise a RFC 9535 query to return a list of possible results (nodelists).
  -- If there is one item in the list, the query is deterministic.Applicative
  -- If there is more than one item in the list, the query is non-deterministic.
-type Query = Value -> [Nodelist]
+type Query = Nodelist -> [Nodelist]
 
 -- root is a query corresponding to $
 -- It is deterministic, so it returns one nodelist.
-root :: Query
+root :: Value -> [Nodelist]
 root v = [[v]]
 
 -- childWildcard is a query corresponding to [*]
 -- It is non-deterministic when applied to an object with more than one member.
+-- WIP: need to upgrade the implementation to take a Nodelist as input
+-- The difficulty is how to combine the non-deterministic effects produced by each node in the nodelist.
+-- Is it some kind of Cartesian product of the non-deterministic lists?
 childWildcard :: Query
-childWildcard (Object o) = permutations $ elems o
-childWildcard (Array a) = [toList a]
-childWildcard (String _) = [[]]
-childWildcard (Number _) = [[]]
-childWildcard (Bool _) = [[]]
-childWildcard Null = [[]]
+childWildcard [] = [[]]
+childWildcard (n:ns) = do
+    l <- cw n
+    r <- childWildcard ns
+    [l, r]
+    where cw :: Value -> [Nodelist] -- input value must be a child of the argument
+          cw (Object o) = permutations $ elems o
+          cw (Array a) = [toList a]
+          cw _ = [[]]
