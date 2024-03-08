6d59ca71515255e859fd83ab41cd41aff92bd775
Glyn Normington <glyn.normington@gmail.com>
2024-03-08 11:45:03 +0000

    Tidy up

--- app/Main.hs
+++ app/Main.hs
@@ -14,19 +14,21 @@ main :: IO ()
 
 main = do
          mapM_ (BSL.putStrLn . encode . uniq) ([
-            do -- [*] applied to an object
-              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"foo\": 123, \"bar\": 456}")
+            do -- [*] applied to an object (dervied from $.o[*] example of Table 6 in RFC 9535)
+              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"j\": 1, \"k\": 2}")
               childWildcard nl,
-            do -- [*][*] applied to an object of objects
-              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
-              nl' :: Nodelist <- childWildcard nl
-              childWildcard nl',
-            do -- [*,*] applied to an object (derived from non-deterministic example in Table 5 of RFC 9535)
+            do -- [*,*] applied to an object (derived from $.o[*, *] example in Table 6 of RFC 9535)
               nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"j\": 1, \"k\": 2}")
               w1 <- childWildcard nl
               w2 <- childWildcard nl
-              return (w1 ++ w2)
+              return (w1 ++ w2),
+            do -- [*][*] applied to an object of objects
+              nl :: Nodelist <- root $ fromMaybe undefined (decode "{\"x\": {\"a\": 1, \"b\":2 }, \"y\": {\"c\": 3, \"d\": 4}}")
+              nl' :: Nodelist <- childWildcard nl
+              childWildcard nl'
             ] :: [[Nodelist]])
 
+-- Remove duplicates from the input list
+-- The implementation ensures that the result list is sorted
 uniq :: Ord a => [a] -> [a]
 uniq = toList . fromList
