1968aa08288985c2959c5050620b719fc865b2b8
Glyn Normington <glyn.normington@gmail.com>
2024-03-12 03:31:29 +0000

    Make output readable

--- app/Main.hs
+++ app/Main.hs
@@ -7,65 +7,97 @@ module Main (main) where
 import Data.Aeson
 import Data.Aeson.QQ
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Lib
 
+runQuery :: ToJSON a => String -> Value -> ([Nodelist] -> a) -> BLU.ByteString
+runQuery desc arg lam = (addNewline $ BLU.fromString desc) `BSL.append` addNewline (encode arg) `BSL.append` encode (lam $ root arg)
 
 main :: IO ()
 
 main = do
-         mapM_ (BSL.putStrLn . addNewline . encode) ([
-            do -- [*] applied to an object (dervied from $.o[*] example of Table 6 in RFC 9535)
-              nl :: Nodelist <- root [aesonQQ| {"j": 1, "k": 2} |]
-              childWildcard nl,
-            do -- [*,*] applied to an object (derived from $.o[*, *] example in Table 6 of RFC 9535)
-              nl :: Nodelist <- root [aesonQQ| {"j": 1, "k": 2} |]
+         mapM_ (BSL.putStrLn . addNewline) [
+          runQuery "[*] applied to an object (derived from $.o[*] example of Table 6 in RFC 9535)"
+          [aesonQQ| {"j": 1, "k": 2} |]
+          (\r -> (do
+              nl :: Nodelist <- r
+              childWildcard nl)),
+
+          runQuery "[*,*] applied to an object (derived from $.o[*, *] example in Table 6 of RFC 9535)"
+          [aesonQQ| {"j": 1, "k": 2} |]
+          (\r -> do
+              nl :: Nodelist <- r
               w1 <- childWildcard nl
               w2 <- childWildcard nl
-              return (w1 ++ w2),
-            do -- [*][*] applied to an object of objects
-              nl :: Nodelist <- root [aesonQQ| { "x": {"a": 1, "b": 2}
-                                               , "y": {"c": 3, "d": 4}
-                                               } |]
+              return (w1 ++ w2)),
+
+          runQuery "[*][*] applied to an object of objects"
+          [aesonQQ| { "x": {"a": 1, "b": 2}
+                    , "y": {"c": 3, "d": 4}
+                    } |]
+          (\r -> do
+              nl :: Nodelist <- r
               nl' :: Nodelist <- childWildcard nl
-              childWildcard nl',
-            do -- [*][*] applied to an object of arrays/objects
-              nl :: Nodelist <- root [aesonQQ| { "x": {"a": 1, "b": 2}
-                                               , "y": [3, 4]
-                                               } |]
+              childWildcard nl'),
+          
+          runQuery "[*][*] applied to an object of arrays/objects"
+          [aesonQQ| { "x": {"a": 1, "b": 2}
+                    , "y": [3, 4]
+                    } |]
+          (\r -> do 
+              nl :: Nodelist <- r
               nl' :: Nodelist <- childWildcard nl
-              childWildcard nl',
-            do -- [*][*] applied to an array of objects
-              nl :: Nodelist <- root [aesonQQ| [ {"a": 1, "b": 2}
-                                               , {"c": 3, "d": 4}
-                                               ] |]
+              childWildcard nl'),
+          
+          runQuery "[*][*] applied to an array of objects"
+          [aesonQQ| [ {"a": 1, "b": 2}
+                    , {"c": 3, "d": 4}
+                    ] |]
+          (\r -> do
+              nl :: Nodelist <- r
               nl' :: Nodelist <- childWildcard nl
-              childWildcard nl',
-            do -- ..[*] applied to an array containing a non-empty array and something else
-              nl :: Nodelist <- root [aesonQQ| [ [1]
-                                               , 2
-                                               ] |]
-              descendantWildcard nl,
-            do -- ..[*] applied to an object 
-              nl :: Nodelist <- root [aesonQQ| { "x": 1
-                                               , "y": 2
-                                               } |]
-              descendantWildcard nl,
-            do -- ..[*] applied to an object containing arrays
-              nl :: Nodelist <- root [aesonQQ| { "x": [1]
-                                               , "y": [2]
-                                               } |]
-              descendantWildcard nl,
-            do -- ..[*] applied to an object containing an object and an array
-              nl :: Nodelist <- root [aesonQQ| { "x": {"a": 1}
-                                               , "y": [3]
-                                               } |]
-              descendantWildcard nl,
-            do -- ..[*] applied to an object containing objects
-              nl :: Nodelist <- root [aesonQQ| { "x": {"a": 1}
-                                               , "y": {"c": 3}
-                                               } |]
-              descendantWildcard nl
-            ] :: [[Nodelist]])
+              childWildcard nl'),
+          
+          runQuery "..[*] applied to an array containing a non-empty array and something else"
+          [aesonQQ| [ [1]
+                    , 2
+                    ] |]
+          (\r -> do
+              nl :: Nodelist <- r
+              descendantWildcard nl),
+          
+          runQuery "..[*] applied to an object"
+          [aesonQQ| { "x": 1
+                    , "y": 2
+                    } |]
+          (\r -> do 
+              nl :: Nodelist <- r
+              descendantWildcard nl),
+          
+          runQuery "..[*] applied to an object containing arrays"
+          [aesonQQ| { "x": [1]
+                    , "y": [2]
+                    } |]
+          (\r -> do
+              nl :: Nodelist <- r
+              descendantWildcard nl),
+          
+          runQuery "..[*] applied to an object containing an object and an array"
+          [aesonQQ| { "x": {"a": 1}
+                    , "y": [3]
+                    } |]
+          (\r -> do
+              nl :: Nodelist <- r
+              descendantWildcard nl),
+          
+          runQuery "..[*] applied to an object containing objects"
+          [aesonQQ| { "x": {"a": 1}
+                    , "y": {"c": 3}
+                    } |]
+          (\r -> do
+              nl :: Nodelist <- r
+              descendantWildcard nl)
+            ]
 
 addNewline :: BSL.ByteString -> BSL.ByteString
 addNewline bs = BSL.snoc bs '\n'
